0b316080985780c6b7f3d4631d21044f74f645b9
Alesya Alekseeva <111797465+Alesya09@users.noreply.github.com>
2023-09-12 00:04:56 -0500

    modify_code (#3)

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
     baseUrl: 'https://www.gamesforthebrain.com/game/checkers/',
     setupNodeEvents(on, config) {
